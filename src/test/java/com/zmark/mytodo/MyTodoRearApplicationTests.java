895e56be75a8853f21e53d92f4f481c668a3490c
zmark <351479188@qq.com>
2023-10-24 21:04:44 +0800

    test:完善测试testCreateNewTagException，新增测试testFindAllTags

--- src/test/java/com/zmark/mytodo/MyTodoRearApplicationTests.java
+++ src/test/java/com/zmark/mytodo/MyTodoRearApplicationTests.java
@@ -29,10 +29,33 @@ class MyTodoRearApplicationTests {
 	@Test
 	void testCreateNewTagException() {
 		try {
+			log.info("尝试创建tag1/tag2/tag3");
 			tagService.createNewTag("tag1/tag2/tag3");
 			log.info("tag1/tag2/tag3 create succeed");
+		} catch (Exception e) {
+			log.error("tag1/tag2/tag3 create failed", e);
+		}
+
+		try {
+			log.info("尝试创建tag1/tag2/tag5");
+			tagService.createNewTag("tag1/tag2/tag5");
+			log.info("tag1/tag2/tag5 create succeed");
+		} catch (Exception e) {
+			log.error("tag1/tag2/tag5 create failed", e);
+		}
+
+		try {
+			log.info("尝试创建tag1/tag3/tag4");
 			tagService.createNewTag("tag1/tag3/tag4");
 			log.error("tag1/tag3/tag4 created but it shouldn't!!!");
+		} catch (Exception e) {
+			log.warn("创建tag失败：", e);
+		}
+
+		try {
+			log.info("尝试创建tag6/tag6/tag90");
+			tagService.createNewTag("tag6/tag6/tag90");
+			log.error("tag6/tag6/tag90 created but it shouldn't!!!");
 		} catch (Exception e) {
 			log.warn("创建tag失败：", e);
 		}
@@ -48,6 +71,15 @@ class MyTodoRearApplicationTests {
 
 	@Test
 	void testFindAllTagsWithNoData() {
+		tagService.findAllTags().forEach(log::info);
+	}
+
+	@Test
+	void testFindAllTags() {
+		tagService.createNewTag("tag1/tag2/tag3");
+		tagService.createNewTag("tag1/tag2/tag4");
+		tagService.createNewTag("tag1/tag2/tag5");
+		tagService.createNewTag("tag/tag6/tag7");
 		tagService.findAllTags().forEach(log::info);
 	}
 
