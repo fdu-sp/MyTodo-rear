9783858cff12a36de1e524dd7a90618e3b5836d6
zmark <351479188@qq.com>
2023-12-04 00:00:13 +0800

    feat:TaskService中新增findAllTasksByTag

--- src/main/java/com/zmark/mytodo/dao/TaskTagMatchDAO.java
+++ src/main/java/com/zmark/mytodo/dao/TaskTagMatchDAO.java
@@ -3,5 +3,8 @@ package com.zmark.mytodo.dao;
 import com.zmark.mytodo.entity.TaskTagMatch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskTagMatchDAO extends JpaRepository<TaskTagMatch, Integer> {
+    List<TaskTagMatch> findAllByTagId(Integer tagId);
 }
--- src/main/java/com/zmark/mytodo/service/TaskService.java
+++ src/main/java/com/zmark/mytodo/service/TaskService.java
@@ -1,10 +1,14 @@
 package com.zmark.mytodo.service;
 
 import com.zmark.mytodo.dao.TaskDAO;
+import com.zmark.mytodo.dao.TaskTagMatchDAO;
+import com.zmark.mytodo.entity.Tag;
 import com.zmark.mytodo.entity.Task;
+import com.zmark.mytodo.entity.TaskTagMatch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,10 +18,12 @@ import java.util.List;
 @Service
 public class TaskService {
     private final TaskDAO taskDAO;
+    private final TaskTagMatchDAO taskTagMatchDAO;
 
     @Autowired
-    public TaskService(TaskDAO taskDAO) {
+    public TaskService(TaskDAO taskDAO, TaskTagMatchDAO taskTagMatchDAO) {
         this.taskDAO = taskDAO;
+        this.taskTagMatchDAO = taskTagMatchDAO;
     }
 
     public Task findTaskById(int taskId) {
@@ -30,5 +36,14 @@ public class TaskService {
 
     public void createNewTask(Task task) {
         taskDAO.save(task);
+    }
+
+    public List<Task> findAllTasksByTag(Tag tag) {
+        List<Task> taskList = new ArrayList<>();
+        List<TaskTagMatch> matchList = taskTagMatchDAO.findAllByTagId(tag.getId());
+        for (TaskTagMatch match : matchList) {
+            taskList.add(findTaskById(match.getTaskId()));
+        }
+        return taskList;
     }
 }
