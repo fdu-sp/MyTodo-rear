e4a97cba9c2a24e66feb18b775a10c60f8aae044
ZMark <351479188@qq.com>
2023-12-04 10:38:47 +0800

    perf:Task类成员变量空行

--- src/main/java/com/zmark/mytodo/entity/Task.java
+++ src/main/java/com/zmark/mytodo/entity/Task.java
@@ -19,25 +19,34 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
+
     @Column(nullable = false, name = "title")
     private String title;
+
     @Column(nullable = false, name = "completed")
     private Boolean completed;
+
     @Column(nullable = false, name = "completed_time")
     private Timestamp completedTime;
+
     @Column(nullable = false, name = "archived")
     private Boolean archived;
+    
     @OneToOne
     @JoinColumn(name = "task_content_info_id", insertable = true, updatable = true)
     private TaskContentInfo taskContentInfo;
+
     @OneToOne
     @JoinColumn(name = "task_priority_info_id", insertable = true, updatable = true)
     private TaskPriorityInfo taskPriorityInfo;
+
     @OneToOne
     @JoinColumn(name = "task_time_info_id", insertable = true, updatable = true)
     private TaskTimeInfo taskTimeInfo;
+
     @Column(nullable = false, name = "create_time")
     private Timestamp createTime;
+
     @Column(nullable = false, name = "update_time")
     private Timestamp updateTime;
 }
