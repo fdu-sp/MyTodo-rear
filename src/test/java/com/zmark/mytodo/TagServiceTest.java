e680466d30c853371276b82a9f84b5ddad80689b
ZMark <351479188@qq.com>
2023-12-04 20:30:05 +0800

    test:TagServiceTest新增测试findAllTagsWithAllChildren

--- src/test/java/com/zmark/mytodo/TagServiceTest.java
+++ src/test/java/com/zmark/mytodo/TagServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -56,6 +57,31 @@ public class TagServiceTest {
             log.info("tag {} saved", tag.getTagName());
             return tag;
         });
+        when(tagDAO.findAll()).thenAnswer(invocation -> {
+            log.info("findAll invoked");
+            return existTagMap.values().stream().toList();
+        });
+        when(tagDAO.findTagsByParentTagId(anyLong())).thenAnswer(invocation -> {
+            Long parentTagId = invocation.getArgument(0);
+            log.info("findTagsByParentTagId invoked, parentTagId: {}", parentTagId);
+            return existTagMap.values().stream()
+                    .filter(tag -> tag.getParentTagId() != null && tag.getParentTagId().equals(parentTagId))
+                    .toList();
+        });
+        when(tagDAO.findAllByParentTagIsNull()).thenAnswer(invocation -> {
+            log.info("findAllByParentTagIsNull invoked");
+            return existTagMap.values().stream()
+                    .filter(tag -> tag.getParentTagId() == null)
+                    .toList();
+        });
+        when(tagDAO.findTagById(anyLong())).thenAnswer(invocation -> {
+            Long tagId = invocation.getArgument(0);
+            log.info("findTagById invoked, tagId: {}", tagId);
+            return existTagMap.values().stream()
+                    .filter(tag -> tag.getId().equals(tagId))
+                    .findFirst()
+                    .orElse(null);
+        });
     }
 
     @Test
@@ -75,6 +101,8 @@ public class TagServiceTest {
         Assertions.assertDoesNotThrow(() -> tagService.createNewTag("tag/tag6/tag7"));
         verify(tagDAO, times(12)).findByTagName(anyString());
         verify(tagDAO, times(8)).save(any(Tag.class));
+
+        Assertions.assertEquals(8, existTagMap.size());
     }
 
     @Test
@@ -117,20 +145,66 @@ public class TagServiceTest {
         tagService.createNewTag("tag1/tag2/tag3");
         tagService.createNewTag("tag1/tag2/tag4");
         tagService.createNewTag("tag1/tag2/tag5");
-        tagDAO.findAll().forEach(tag -> log.info(TagDTO.from(tag).toString()));
+        List<Tag> tagList = tagDAO.findAll();
+        Assertions.assertEquals(5, tagList.size());
+        Assertions.assertEquals(1, tagList.stream().filter(tag -> tag.getTagName().equals("tag1")).count());
+        Assertions.assertEquals(1, tagList.stream().filter(tag -> tag.getTagName().equals("tag2")).count());
+        Assertions.assertEquals(1, tagList.stream().filter(tag -> tag.getTagName().equals("tag3")).count());
+        Assertions.assertEquals(1, tagList.stream().filter(tag -> tag.getTagName().equals("tag4")).count());
+        Assertions.assertEquals(1, tagList.stream().filter(tag -> tag.getTagName().equals("tag5")).count());
+        tagList.forEach(tag -> log.info(TagDTO.from(tag).toString()));
     }
 
     @Test
     void testFindAllTagsWithNoData() {
-        tagService.findAllTagsWithAllChildren().forEach(tagDTO -> log.info(tagDTO.toString()));
+        List<TagDTO> tagDTOList = tagService.findAllTagsWithAllChildren();
+        Assertions.assertEquals(0, tagDTOList.size());
     }
 
     @Test
-    void testFindAllTags() throws NewEntityException {
-        tagService.createNewTag("tag1/tag2/tag3");
-        tagService.createNewTag("tag1/tag2/tag4");
-        tagService.createNewTag("tag1/tag2/tag5");
-        tagService.createNewTag("tag/tag6/tag7");
-        tagService.findAllTagsWithAllChildren().forEach(TagDTO -> log.info(TagDTO.toString()));
+    void testFindAllTagsWithAllChildren() throws NewEntityException {
+        // 准备数据：
+        tagService.createNewTag("大学/课程/智能移动平台开发");
+        tagService.createNewTag("大学/课程/软件设计");
+        tagService.createNewTag("大学/课程/数据库设计（H）");
+        tagService.createNewTag("大学/课程/操作系统（H）");
+        tagService.createNewTag("大学/课程/操作系统（H）/操作系统实验");
+
+        // 测试：
+        List<TagDTO> tagDTOList = tagService.findAllTagsWithAllChildren();
+
+        // 断言：
+        Assertions.assertEquals(7, tagDTOList.size());
+        tagDTOList.stream().filter(tagDTO -> tagDTO.getTagName().equals("大学")).findFirst().ifPresent(tagDTO -> {
+            Assertions.assertEquals(1, tagDTO.getChildren().size());
+            Assertions.assertEquals("大学", tagDTO.getTagPath());
+            tagDTO.getChildren().stream().filter(tagDTO1 -> tagDTO1.getTagName().equals("课程")).findFirst().ifPresent(tagDTO1 -> {
+                Assertions.assertEquals(4, tagDTO1.getChildren().size());
+                Assertions.assertEquals("大学/课程", tagDTO1.getTagPath());
+                tagDTO1.getChildren().stream().filter(tagDTO2 -> tagDTO2.getTagName().equals("操作系统（H）")).findFirst().ifPresent(tagDTO2 -> {
+                    Assertions.assertEquals(1, tagDTO2.getChildren().size());
+                    Assertions.assertEquals("大学/课程/操作系统（H）", tagDTO2.getTagPath());
+                    tagDTO2.getChildren().stream().filter(tagDTO3 -> tagDTO3.getTagName().equals("操作系统实验")).findFirst().ifPresent(tagDTO3 -> {
+                        Assertions.assertEquals(0, tagDTO3.getChildren().size());
+                        Assertions.assertEquals("大学/课程/操作系统（H）/操作系统实验", tagDTO3.getTagPath());
+                    });
+                });
+            });
+        });
+        tagDTOList.stream().filter(tagDTO -> tagDTO.getTagName().equals("智能移动平台开发")).findFirst().ifPresent(tagDTO -> {
+            Assertions.assertEquals(0, tagDTO.getChildren().size());
+            Assertions.assertEquals("大学/课程/智能移动平台开发", tagDTO.getTagPath());
+        });
+        tagDTOList.stream().filter(tagDTO -> tagDTO.getTagName().equals("软件设计")).findFirst().ifPresent(tagDTO -> {
+            Assertions.assertEquals(0, tagDTO.getChildren().size());
+            Assertions.assertEquals("大学/课程/软件设计", tagDTO.getTagPath());
+        });
+        tagDTOList.stream().filter(tagDTO -> tagDTO.getTagName().equals("数据库设计（H）")).findFirst().ifPresent(tagDTO -> {
+            Assertions.assertEquals(0, tagDTO.getChildren().size());
+            Assertions.assertEquals("大学/课程/数据库设计（H）", tagDTO.getTagPath());
+        });
+
+        // 打印：
+        tagDTOList.forEach(TagDTO -> log.info(TagDTO.toString()));
     }
 }
