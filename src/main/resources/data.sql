b6e1118090769cff8ee980c1dc1c6998b88dc7ff
zmark <351479188@qq.com>
2023-12-06 00:15:55 +0800

    test:新增更多测试数据

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,32 +1,31 @@
 INSERT INTO task (id, title, completed, completed_time, archived, create_time, update_time)
-VALUES (1, 'test1', 0, null, 0, '2023-12-05 16:15:21', '2023-12-05 16:15:21');
+VALUES (1, '概率论与数理统计HW11', 0, null, 0, '2023-12-05 16:15:21', '2023-12-05 16:15:21');
 INSERT INTO task (id, title, completed, completed_time, archived, create_time, update_time)
-VALUES (2, 'test2', 0, null, 0, '2023-12-05 16:15:21', '2023-12-05 16:15:21');
+VALUES (2, '《安卓开发》Lab4', 0, null, 0, '2023-12-05 16:15:21', '2023-12-05 16:15:21');
 INSERT INTO task (id, title, completed, completed_time, archived, create_time, update_time)
 VALUES (3, '《安卓开发》Lab3', 0, null, 0, '2023-12-05 16:16:05', '2023-12-05 16:16:05');
 
 INSERT INTO task_content_info (id, task_id, description, create_time, update_time)
-VALUES (1, 1, 'test1', '2023-12-05 16:15:21', '2023-12-05 16:15:21');
+VALUES (1, 1, 'Lab2', '2023-12-05 16:15:21', '2023-12-05 16:15:21');
 INSERT INTO task_content_info (id, task_id, description, create_time, update_time)
-VALUES (2, 2, 'test2', '2023-12-05 16:15:21', '2023-12-05 16:15:21');
+VALUES (2, 2, 'Lab4', '2023-12-05 16:15:21', '2023-12-05 16:15:21');
 INSERT INTO task_content_info (id, task_id, description, create_time, update_time)
 VALUES (3, 3, '', '2023-12-05 16:16:05', '2023-12-05 16:16:05');
 
-
 INSERT INTO task_priority_info (id, task_id, is_important, is_urgent)
-VALUES (1, 1, 0, 0);
+VALUES (1, 1, 1, 0);
 INSERT INTO task_priority_info (id, task_id, is_important, is_urgent)
-VALUES (2, 2, 0, 0);
+VALUES (2, 2, 1, 0);
 INSERT INTO task_priority_info (id, task_id, is_important, is_urgent)
 VALUES (3, 3, 1, 1);
 
 
 INSERT INTO task_time_info (id, task_id, end_date, end_time, activate_countdown, expected_execution_date,
                             expected_execution_start_period, expected_execution_end_period)
-VALUES (1, 1, null, null, 0, null, null, null);
+VALUES (1, 1, '2023-10-20', '23:59:59', 0, '2023-12-09', '08:00:00', '12:00:00');
 INSERT INTO task_time_info (id, task_id, end_date, end_time, activate_countdown, expected_execution_date,
                             expected_execution_start_period, expected_execution_end_period)
-VALUES (2, 2, null, null, 0, null, null, null);
+VALUES (2, 2, '2023-12-20', '23:59:59', 0, '2023-12-09', '08:00:00', '12:00:00');
 INSERT INTO task_time_info (id, task_id, end_date, end_time, activate_countdown, expected_execution_date,
                             expected_execution_start_period, expected_execution_end_period)
 VALUES (3, 3, '2023-12-20', '23:59:59', 0, '2023-12-09', '08:00:00', '12:00:00');
@@ -40,6 +39,135 @@ INSERT INTO tag (id, tag_name, parent_tag_id, create_time, update_time)
 VALUES (3, '智能移动平台开发', 2, '2023-12-05 16:16:05', '2023-12-05 16:16:05');
 INSERT INTO tag (id, tag_name, parent_tag_id, create_time, update_time)
 VALUES (4, '操作系统（H)', 2, '2023-12-05 16:16:05', '2023-12-05 16:16:05');
+INSERT INTO tag (id, tag_name, parent_tag_id, create_time, update_time)
+VALUES (5, '概率论与数理统计', 2, '2023-12-05 16:16:05', '2023-12-05 16:16:05');
 
 INSERT INTO task_tag_match (id, task_id, tag_id)
-VALUES (1, 3, 3);
+VALUES (1, 1, 5),
+       (2, 2, 3),
+       (3, 3, 3);
+
+-- Task 4
+INSERT INTO task (id, title, completed, completed_time, archived, create_time, update_time)
+VALUES (4, '计算机网络作业', 0, null, 0, '2023-12-05 17:30:00', '2023-12-05 17:30:00');
+
+INSERT INTO task_content_info (id, task_id, description, create_time, update_time)
+VALUES (4, 4, 'Chapter 5 Exercises', '2023-12-05 17:30:00', '2023-12-05 17:30:00');
+
+INSERT INTO task_priority_info (id, task_id, is_important, is_urgent)
+VALUES (4, 4, 1, 0);
+
+INSERT INTO task_time_info (id, task_id, end_date, end_time, activate_countdown, expected_execution_date,
+                            expected_execution_start_period, expected_execution_end_period)
+VALUES (4, 4, '2023-12-15', '23:59:59', 0, '2023-12-09', '10:00:00', '14:00:00');
+
+INSERT INTO tag (id, tag_name, parent_tag_id, create_time, update_time)
+VALUES (6, '计算机网络', 2, '2023-12-05 17:30:00', '2023-12-05 17:30:00');
+
+INSERT INTO task_tag_match (id, task_id, tag_id)
+VALUES (4, 4, 6);
+
+
+-- Task 5
+INSERT INTO task (id, title, completed, completed_time, archived, create_time, update_time)
+VALUES (5, '人工智能实验', 0, null, 0, '2023-12-05 18:00:00', '2023-12-05 18:00:00');
+
+INSERT INTO task_content_info (id, task_id, description, create_time, update_time)
+VALUES (5, 5, 'Decision Trees Implementation', '2023-12-05 18:00:00', '2023-12-05 18:00:00');
+
+INSERT INTO task_priority_info (id, task_id, is_important, is_urgent)
+VALUES (5, 5, 1, 1);
+
+INSERT INTO task_time_info (id, task_id, end_date, end_time, activate_countdown, expected_execution_date,
+                            expected_execution_start_period, expected_execution_end_period)
+VALUES (5, 5, '2023-12-18', '23:59:59', 0, '2023-12-10', '09:00:00', '13:00:00');
+
+INSERT INTO tag (id, tag_name, parent_tag_id, create_time, update_time)
+VALUES (7, '人工智能', 2, '2023-12-05 18:00:00', '2023-12-05 18:00:00');
+
+INSERT INTO task_tag_match (id, task_id, tag_id)
+VALUES (5, 5, 7);
+
+
+-- Task 6
+INSERT INTO task (id, title, completed, completed_time, archived, create_time, update_time)
+VALUES (6, '软件测试项目', 0, null, 0, '2023-12-05 18:30:00', '2023-12-05 18:30:00');
+
+INSERT INTO task_content_info (id, task_id, description, create_time, update_time)
+VALUES (6, 6, 'Test Case Design', '2023-12-05 18:30:00', '2023-12-05 18:30:00');
+
+INSERT INTO task_priority_info (id, task_id, is_important, is_urgent)
+VALUES (6, 6, 0, 1);
+
+INSERT INTO task_time_info (id, task_id, end_date, end_time, activate_countdown, expected_execution_date,
+                            expected_execution_start_period, expected_execution_end_period)
+VALUES (6, 6, '2023-12-22', '23:59:59', 0, '2023-12-12', '14:00:00', '18:00:00');
+
+INSERT INTO tag (id, tag_name, parent_tag_id, create_time, update_time)
+VALUES (8, '软件测试', 2, '2023-12-05 18:30:00', '2023-12-05 18:30:00');
+
+INSERT INTO task_tag_match (id, task_id, tag_id)
+VALUES (6, 6, 8);
+
+-- Task 7
+INSERT INTO task (id, title, completed, completed_time, archived, create_time, update_time)
+VALUES (7, '算法设计作业', 0, null, 0, '2023-12-06 09:45:00', '2023-12-06 09:45:00');
+
+INSERT INTO task_content_info (id, task_id, description, create_time, update_time)
+VALUES (7, 7, 'Dynamic Programming Problems', '2023-12-06 09:45:00', '2023-12-06 09:45:00');
+
+INSERT INTO task_priority_info (id, task_id, is_important, is_urgent)
+VALUES (7, 7, 1, 1);
+
+INSERT INTO task_time_info (id, task_id, end_date, end_time, activate_countdown, expected_execution_date,
+                            expected_execution_start_period, expected_execution_end_period)
+VALUES (7, 7, '2023-12-15', '23:59:59', 0, '2023-12-09', '11:00:00', '15:00:00');
+
+INSERT INTO tag (id, tag_name, parent_tag_id, create_time, update_time)
+VALUES (9, '算法', 2, '2023-12-06 09:45:00', '2023-12-06 09:45:00');
+
+INSERT INTO task_tag_match (id, task_id, tag_id)
+VALUES (7, 7, 9);
+
+
+-- Task 8
+INSERT INTO task (id, title, completed, completed_time, archived, create_time, update_time)
+VALUES (8, '数据库系统实验', 0, null, 0, '2023-12-06 10:30:00', '2023-12-06 10:30:00');
+
+INSERT INTO task_content_info (id, task_id, description, create_time, update_time)
+VALUES (8, 8, 'Normalization and Indexing', '2023-12-06 10:30:00', '2023-12-06 10:30:00');
+
+INSERT INTO task_priority_info (id, task_id, is_important, is_urgent)
+VALUES (8, 8, 0, 1);
+
+INSERT INTO task_time_info (id, task_id, end_date, end_time, activate_countdown, expected_execution_date,
+                            expected_execution_start_period, expected_execution_end_period)
+VALUES (8, 8, '2023-12-20', '23:59:59', 0, '2023-12-10', '10:00:00', '14:00:00');
+
+INSERT INTO tag (id, tag_name, parent_tag_id, create_time, update_time)
+VALUES (10, '数据库', 2, '2023-12-06 10:30:00', '2023-12-06 10:30:00');
+
+INSERT INTO task_tag_match (id, task_id, tag_id)
+VALUES (8, 8, 10);
+
+
+-- Task 9
+INSERT INTO task (id, title, completed, completed_time, archived, create_time, update_time)
+VALUES (9, '计算机图形学项目', 0, null, 0, '2023-12-06 11:15:00', '2023-12-06 11:15:00');
+
+INSERT INTO task_content_info (id, task_id, description, create_time, update_time)
+VALUES (9, 9, '3D Rendering Implementation', '2023-12-06 11:15:00', '2023-12-06 11:15:00');
+
+INSERT INTO task_priority_info (id, task_id, is_important, is_urgent)
+VALUES (9, 9, 1, 0);
+
+INSERT INTO task_time_info (id, task_id, end_date, end_time, activate_countdown, expected_execution_date,
+                            expected_execution_start_period, expected_execution_end_period)
+VALUES (9, 9, '2023-12-25', '23:59:59', 0, '2023-12-15', '13:00:00', '17:00:00');
+
+INSERT INTO tag (id, tag_name, parent_tag_id, create_time, update_time)
+VALUES (11, '计算机图形学', 2, '2023-12-06 11:15:00', '2023-12-06 11:15:00');
+
+INSERT INTO task_tag_match (id, task_id, tag_id)
+VALUES (9, 9, 11);
+
