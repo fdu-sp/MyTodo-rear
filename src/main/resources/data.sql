660988cd7d2affef55a4abbf1513ecaa673d8421
zmark <351479188@qq.com>
2023-12-05 15:29:13 +0800

    test:data.sql新增测试数据

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,15 @@
 INSERT INTO task (title, completed, archived)
 VALUES ('test1', 0, 0),
        ('test2', 0, 0);
+
+INSERT INTO task_content_info (task_id, description)
+VALUES (1, 'test1'),
+       (2, 'test2');
+
+INSERT INTO task_time_info (task_id)
+VALUES (1),
+       (2);
+
+INSERT INTO task_priority_info (task_id)
+VALUES (1),
+       (2);
