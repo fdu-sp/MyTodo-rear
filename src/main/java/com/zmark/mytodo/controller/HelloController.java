9a90db151b387168d7ab767a87bcfe091f17daf9
zmark <351479188@qq.com>
2023-12-05 17:45:04 +0800

    debug:添加debug语句

--- src/main/java/com/zmark/mytodo/controller/HelloController.java
+++ src/main/java/com/zmark/mytodo/controller/HelloController.java
@@ -2,14 +2,17 @@ package com.zmark.mytodo.controller;
 
 import com.zmark.mytodo.result.Result;
 import com.zmark.mytodo.result.ResultFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping("/api/hello")
     public Result hello() {
-        return ResultFactory.buildSuccessResult("Hello World!", "Hello World!");
+        log.debug("Hello World! api called!");
+        return ResultFactory.buildSuccessResult("Hello World!", "Hello ZMark!");
     }
 }
