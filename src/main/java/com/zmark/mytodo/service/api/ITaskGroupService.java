a4ace3703e8d755322087a7f33db577c9fec89c9
zmark <351479188@qq.com>
2023-12-08 14:38:59 +0800

    feat:设计 ITaskGroupService

--- src/main/java/com/zmark/mytodo/service/api/ITaskGroupService.java
+++ src/main/java/com/zmark/mytodo/service/api/ITaskGroupService.java
@@ -1,6 +1,8 @@
 package com.zmark.mytodo.service.api;
 
-import com.zmark.mytodo.entity.TaskGroup;
+import com.zmark.mytodo.dto.group.TaskGroupSimpleDTO;
+import com.zmark.mytodo.exception.RepeatedEntityInDatabase;
+import com.zmark.mytodo.vo.group.req.TaskGroupCreateReq;
 
 import java.util.List;
 
@@ -9,10 +11,7 @@ import java.util.List;
  * @date 2023/12/7 19:25
  */
 public interface ITaskGroupService {
+    List<TaskGroupSimpleDTO> findAll();
 
-    TaskGroup findById(long taskGroupId);
-
-    TaskGroup findByName(String name);
-
-    List<TaskGroup> findAll();
+    TaskGroupSimpleDTO createNew(TaskGroupCreateReq createReq) throws RepeatedEntityInDatabase;
 }
---
+++ src/main/java/com/zmark/mytodo/vo/group/req/TaskGroupCreateReq.java
@@ -0,0 +1,28 @@
+package com.zmark.mytodo.vo.group.req;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author ZMark
+ * @date 2023/12/8 14:24
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskGroupCreateReq {
+    @NotNull(message = "分组名称必填")
+    @NotEmpty(message = "分组名称不能为空")
+    String name;
+
+    /**
+     * 必填，但是可以为空
+     */
+    @NotNull(message = "分组描述必填")
+    String description;
+}
