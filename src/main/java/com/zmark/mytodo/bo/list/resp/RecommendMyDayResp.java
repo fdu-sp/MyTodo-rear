491781669701ab7651576fda861d646b11a899af
zmark <351479188@qq.com>
2023-12-11 22:36:02 +0800

    refactor:类的成员变量使用文档注释

--- src/main/java/com/zmark/mytodo/bo/list/resp/RecommendMyDayResp.java
+++ src/main/java/com/zmark/mytodo/bo/list/resp/RecommendMyDayResp.java
@@ -14,14 +14,28 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RecommendMyDayResp {
-    // 截止日期为今天的任务
+    /**
+     * 截止日期为今天的任务
+     */
     RecommendTaskListResp tasksEndToday;
-    // 截止日期为之后三天的任务
+
+    /**
+     * 截止日期为之后三天的任务
+     */
     RecommendTaskListResp tasksEndInThreeDays;
-    // 截止日期为之后四到七天的任务
+
+    /**
+     * 截止日期为之后四到七天的任务
+     */
     RecommendTaskListResp tasksEndInFourToSevenDays;
-    // 已经过期，但是没有完成的任务
+
+    /**
+     * 已经过期，但是没有完成的任务
+     */
     RecommendTaskListResp uncompletedTasksEndBeforeToday;
-    // 最新一天创建的任务
+
+    /**
+     * 最新一天创建的任务
+     */
     RecommendTaskListResp latestCreatedTasks;
 }
