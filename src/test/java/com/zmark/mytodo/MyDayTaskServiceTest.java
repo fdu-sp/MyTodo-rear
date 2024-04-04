fa2acdb6f0d67e53f24e932dd810203ce3eb0fa3
ZMark <351479188@qq.com>
2024-04-04 14:42:03 +0800

    test(MyDayTaskService): test_getRecommendTasks

--- src/test/java/com/zmark/mytodo/MyDayTaskServiceTest.java
+++ src/test/java/com/zmark/mytodo/MyDayTaskServiceTest.java
@@ -3,6 +3,7 @@ package com.zmark.mytodo;
 import com.zmark.mytodo.dao.MyDayTaskDAO;
 import com.zmark.mytodo.dao.TaskDAO;
 import com.zmark.mytodo.dto.list.RecommendMyDayDTO;
+import com.zmark.mytodo.dto.list.RecommendTaskListDTO;
 import com.zmark.mytodo.dto.task.TaskDTO;
 import com.zmark.mytodo.entity.MyDayTask;
 import com.zmark.mytodo.entity.TaskTimeInfo;
@@ -27,10 +28,8 @@ import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.util.AssertionErrors.assertEquals;
-import static org.springframework.test.util.AssertionErrors.assertTrue;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.util.AssertionErrors.*;
 
 /**
  * @author ZMark
@@ -55,6 +54,10 @@ public class MyDayTaskServiceTest {
     public static final Timestamp tomorrowTimestamp = new Timestamp(tomorrow.getTime());
     public static final Date afterThreeDays = Date.valueOf("2024-04-01");
     public static final Timestamp afterThreeDaysTimestamp = new Timestamp(afterThreeDays.getTime());
+    public static final Date afterFourDays = Date.valueOf("2024-04-02");
+    public static final Timestamp afterFourDaysTimestamp = new Timestamp(afterFourDays.getTime());
+    public static final Date afterSevenDays = Date.valueOf("2024-04-05");
+    public static final Timestamp afterSevenDaysTimestamp = new Timestamp(afterSevenDays.getTime());
 
     /**
      * 模拟数据库中的task
@@ -81,6 +84,15 @@ public class MyDayTaskServiceTest {
         // 初始化模拟数据
         tasksInDB = new ArrayList<>();
         myDayTaskListInDB = new HashMap<>();
+        // mock 静态方法 TimeUtils
+        mockStatic(TimeUtils.class);
+        when(TimeUtils.afterDays(1)).thenReturn(tomorrow);
+        when(TimeUtils.afterDays(3)).thenReturn(afterThreeDays);
+        when(TimeUtils.afterDays(4)).thenReturn(afterFourDays);
+        when(TimeUtils.afterDays(7)).thenReturn(afterSevenDays);
+        when(TimeUtils.today()).thenReturn(today);
+        when(TimeUtils.before(3)).thenReturn(beforeThreeDaysTimestamp);
+        when(TimeUtils.now()).thenReturn(todayTimestamp);
     }
 
     @BeforeEach
@@ -108,7 +120,7 @@ public class MyDayTaskServiceTest {
                 .thenAnswer(invocationOnMock -> {
                     Date endDateStart = invocationOnMock.getArgument(0);
                     Date endDateEnd = invocationOnMock.getArgument(1);
-                    log.info("模拟获取{}-{}之间未完成的任务", endDateStart, endDateEnd);
+                    log.info("模拟获取{}到{}之间未完成的任务", endDateStart, endDateEnd);
                     return tasksInDB.stream()
                             .filter(task -> {
                                 if (task.getCompleted()) {
@@ -140,12 +152,11 @@ public class MyDayTaskServiceTest {
                                     return true;
                                 }
                                 Timestamp reminderTimestamp = task.getTaskTimeInfo().getReminderTimestamp();
-                                if (reminderTimestamp == null) {
-                                    return false;
-                                }
-                                Date reminderDate = new Date(reminderTimestamp.getTime());
-                                if (reminderDate.before(beforeDate)) {
-                                    return true;
+                                if (reminderTimestamp != null) {
+                                    Date reminderDate = new Date(reminderTimestamp.getTime());
+                                    if (reminderDate.before(beforeDate)) {
+                                        return true;
+                                    }
                                 }
                                 Date expectedExecuteDate = task.getTaskTimeInfo().getExpectedExecutionDate();
                                 return expectedExecuteDate != null && expectedExecuteDate.before(beforeDate);
@@ -185,14 +196,6 @@ public class MyDayTaskServiceTest {
                     log.info("模拟查询我的一天列表");
                     return new ArrayList<>(myDayTaskListInDB.values());
                 });
-        // todo mock taskDAO?
-        when(taskDAO.findAllByTaskTimeInfo_EndDate(any(Date.class)))
-                .thenAnswer(invocationOnMock -> {
-                    log.info("模拟查询某日截止的任务");
-                    // todo
-                    Date today = TimeUtils.today();
-                    return null;
-                });
     }
 
     /**
@@ -209,25 +212,25 @@ public class MyDayTaskServiceTest {
         // 1. Given: 准备数据
         // 准备Task
         // 截止时间、提醒时间、规划时间在今天的任务
-        TaskDTO taskOfFullTimeInfoOfToday = saveTask(todayTimestamp, todayTimestamp, today, today);
-        TaskDTO taskRemindToday = saveTask(todayTimestamp, todayTimestamp, null, null);
-        TaskDTO taskExpectedToday = saveTask(todayTimestamp, null, today, null);
-        TaskDTO taskEndToday = saveTask(todayTimestamp, null, null, today);
+        TaskDTO taskOfFullTimeInfoOfToday = saveUncompletedTask(todayTimestamp, todayTimestamp, today, today);
+        TaskDTO taskRemindToday = saveUncompletedTask(todayTimestamp, todayTimestamp, null, null);
+        TaskDTO taskExpectedToday = saveUncompletedTask(todayTimestamp, null, today, null);
+        TaskDTO taskEndToday = saveUncompletedTask(todayTimestamp, null, null, today);
         // 截止时间、提醒时间、规划时间在明天的任务
-        TaskDTO taskOfFullTimeInfoOfTomorrow = saveTask(tomorrowTimestamp, tomorrowTimestamp, tomorrow, tomorrow);
-        TaskDTO taskRemindTomorrow = saveTask(tomorrowTimestamp, tomorrowTimestamp, null, null);
-        TaskDTO taskExpectedTomorrow = saveTask(tomorrowTimestamp, null, tomorrow, null);
-        TaskDTO taskEndTomorrow = saveTask(tomorrowTimestamp, null, null, tomorrow);
+        TaskDTO taskOfFullTimeInfoOfTomorrow = saveUncompletedTask(tomorrowTimestamp, tomorrowTimestamp, tomorrow, tomorrow);
+        TaskDTO taskRemindTomorrow = saveUncompletedTask(tomorrowTimestamp, tomorrowTimestamp, null, null);
+        TaskDTO taskExpectedTomorrow = saveUncompletedTask(tomorrowTimestamp, null, tomorrow, null);
+        TaskDTO taskEndTomorrow = saveUncompletedTask(tomorrowTimestamp, null, null, tomorrow);
         // 截止时间、提醒时间、规划时间在昨天的任务
-        TaskDTO taskOfFullTimeInfoOfYesterday = saveTask(yesterdayTimestamp, yesterdayTimestamp, yesterday, yesterday);
-        TaskDTO taskRemindYesterday = saveTask(yesterdayTimestamp, yesterdayTimestamp, null, null);
-        TaskDTO taskExpectedYesterday = saveTask(yesterdayTimestamp, null, yesterday, null);
-        TaskDTO taskEndYesterday = saveTask(yesterdayTimestamp, null, null, yesterday);
+        TaskDTO taskOfFullTimeInfoOfYesterday = saveUncompletedTask(yesterdayTimestamp, yesterdayTimestamp, yesterday, yesterday);
+        TaskDTO taskRemindYesterday = saveUncompletedTask(yesterdayTimestamp, yesterdayTimestamp, null, null);
+        TaskDTO taskExpectedYesterday = saveUncompletedTask(yesterdayTimestamp, null, yesterday, null);
+        TaskDTO taskEndYesterday = saveUncompletedTask(yesterdayTimestamp, null, null, yesterday);
         // 截止时间、提醒时间、规划时间在3天后的任务
-        TaskDTO taskOfFullTimeInfoOfAfterThreeDays = saveTask(beforeThreeDaysTimestamp, afterThreeDaysTimestamp, afterThreeDays, afterThreeDays);
-        TaskDTO taskEndAfterThreeDays = saveTask(beforeThreeDaysTimestamp, null, null, afterThreeDays);
-        TaskDTO taskRemindAfterThreeDays = saveTask(beforeThreeDaysTimestamp, beforeThreeDaysTimestamp, null, null);
-        TaskDTO taskExpectedAfterThreeDays = saveTask(beforeThreeDaysTimestamp, null, afterThreeDays, null);
+        TaskDTO taskOfFullTimeInfoOfAfterThreeDays = saveUncompletedTask(beforeThreeDaysTimestamp, afterThreeDaysTimestamp, afterThreeDays, afterThreeDays);
+        TaskDTO taskEndAfterThreeDays = saveUncompletedTask(beforeThreeDaysTimestamp, null, null, afterThreeDays);
+        TaskDTO taskRemindAfterThreeDays = saveUncompletedTask(beforeThreeDaysTimestamp, beforeThreeDaysTimestamp, null, null);
+        TaskDTO taskExpectedAfterThreeDays = saveUncompletedTask(beforeThreeDaysTimestamp, null, afterThreeDays, null);
 
         // 2. when
         List<TaskDTO> myDayTaskList = myDayTaskService.getMyDayList();
@@ -240,13 +243,38 @@ public class MyDayTaskServiceTest {
         assertTrue("我的一天中应该有今日提醒的任务", myDayTaskList.contains(taskRemindToday));
     }
 
-    private TaskDTO saveTask(Timestamp createTimestamp,
-                             Timestamp reminderTimestamp,
-                             Date expectedExecutionDate,
-                             Date endDate) {
+    private TaskDTO saveUncompletedTask(Timestamp createTimestamp,
+                                        Timestamp reminderTimestamp,
+                                        Date expectedExecutionDate,
+                                        Date endDate) {
         Long taskId = (long) tasksInDB.size();
         TaskDTO taskDTO = TaskDTO.builder().id(taskId)
                 .createTime(createTimestamp)
+                .completed(false)
+                .taskTimeInfo(TaskTimeInfo.builder()
+                        .expectedExecutionDate(expectedExecutionDate)
+                        .reminderTimestamp(reminderTimestamp)
+                        .endDate(endDate)
+                        .build()).build();
+        tasksInDB.add(taskDTO);
+        if ((endDate != null && endDate.equals(today))
+                || (expectedExecutionDate != null && expectedExecutionDate.equals(today))
+                || (reminderTimestamp != null && new Date(reminderTimestamp.getTime()).equals(today))) {
+            // 模拟MyDayTaskScheduler：自动添加到我的一天中
+            myDayTaskListInDB.put(taskId, MyDayTask.builder()
+                    .taskId(taskId).build());
+        }
+        return taskDTO;
+    }
+
+    private TaskDTO saveCompletedTask(Timestamp createTimestamp,
+                                      Timestamp reminderTimestamp,
+                                      Date expectedExecutionDate,
+                                      Date endDate) {
+        Long taskId = (long) tasksInDB.size();
+        TaskDTO taskDTO = TaskDTO.builder().id(taskId)
+                .createTime(createTimestamp)
+                .completed(true)
                 .taskTimeInfo(TaskTimeInfo.builder()
                         .expectedExecutionDate(expectedExecutionDate)
                         .reminderTimestamp(reminderTimestamp)
@@ -275,31 +303,87 @@ public class MyDayTaskServiceTest {
     public void test_getRecommendTasks() {
         // Given 用户已经添加了多个带有不同时间信息的任务
         // 准备Task
-        // 截止时间、提醒时间、规划时间在今天的任务
-        TaskDTO taskOfFullTimeInfoOfToday = saveTask(todayTimestamp, todayTimestamp, today, today);
-        TaskDTO taskRemindToday = saveTask(todayTimestamp, todayTimestamp, null, null);
-        TaskDTO taskExpectedToday = saveTask(todayTimestamp, null, today, null);
-        TaskDTO taskEndToday = saveTask(todayTimestamp, null, null, today);
-        // 截止时间、提醒时间、规划时间在明天的任务
-        TaskDTO taskOfFullTimeInfoOfTomorrow = saveTask(tomorrowTimestamp, tomorrowTimestamp, tomorrow, tomorrow);
-        TaskDTO taskRemindTomorrow = saveTask(tomorrowTimestamp, tomorrowTimestamp, null, null);
-        TaskDTO taskExpectedTomorrow = saveTask(tomorrowTimestamp, null, tomorrow, null);
-        TaskDTO taskEndTomorrow = saveTask(tomorrowTimestamp, null, null, tomorrow);
-        // 截止时间、提醒时间、规划时间在昨天的任务
-        TaskDTO taskOfFullTimeInfoOfYesterday = saveTask(yesterdayTimestamp, yesterdayTimestamp, yesterday, yesterday);
-        TaskDTO taskRemindYesterday = saveTask(yesterdayTimestamp, yesterdayTimestamp, null, null);
-        TaskDTO taskExpectedYesterday = saveTask(yesterdayTimestamp, null, yesterday, null);
-        TaskDTO taskEndYesterday = saveTask(yesterdayTimestamp, null, null, yesterday);
-        // 截止时间、提醒时间、规划时间在3天后的任务
-        TaskDTO taskOfFullTimeInfoOfAfterThreeDays = saveTask(beforeThreeDaysTimestamp, afterThreeDaysTimestamp, afterThreeDays, afterThreeDays);
-        TaskDTO taskEndAfterThreeDays = saveTask(beforeThreeDaysTimestamp, null, null, afterThreeDays);
-        TaskDTO taskRemindAfterThreeDays = saveTask(beforeThreeDaysTimestamp, beforeThreeDaysTimestamp, null, null);
-        TaskDTO taskExpectedAfterThreeDays = saveTask(beforeThreeDaysTimestamp, null, afterThreeDays, null);
+        // 未完成的任务：可能在推荐中
+        // 截止时间、提醒时间、规划时间在今天的任务(默认全部自动加入我的一天，所以都不在推荐中)
+        TaskDTO taskOfFullTimeInfoOfToday = saveUncompletedTask(todayTimestamp, todayTimestamp, today, today);
+        TaskDTO taskRemindToday = saveUncompletedTask(todayTimestamp, todayTimestamp, null, null);
+        TaskDTO taskExpectedToday = saveUncompletedTask(todayTimestamp, null, today, null);
+        TaskDTO taskEndToday = saveUncompletedTask(todayTimestamp, null, null, today);
+        // 截止时间、提醒时间、规划时间在明天的任务 (第一个和第四个在 三天内截止中) (第2、3个在 最近创建)
+        TaskDTO taskOfFullTimeInfoOfTomorrow = saveUncompletedTask(todayTimestamp, tomorrowTimestamp, tomorrow, tomorrow);
+        TaskDTO taskRemindTomorrow = saveUncompletedTask(todayTimestamp, tomorrowTimestamp, null, null);
+        TaskDTO taskExpectedTomorrow = saveUncompletedTask(todayTimestamp, null, tomorrow, null);
+        TaskDTO taskEndTomorrow = saveUncompletedTask(todayTimestamp, null, null, tomorrow);
+        // 截止时间、提醒时间、规划时间在昨天的任务（4个都在 过期）
+        TaskDTO taskOfFullTimeInfoOfYesterday = saveUncompletedTask(yesterdayTimestamp, yesterdayTimestamp, yesterday, yesterday);
+        TaskDTO taskRemindYesterday = saveUncompletedTask(yesterdayTimestamp, yesterdayTimestamp, null, null);
+        TaskDTO taskExpectedYesterday = saveUncompletedTask(yesterdayTimestamp, null, yesterday, null);
+        TaskDTO taskEndYesterday = saveUncompletedTask(yesterdayTimestamp, null, null, yesterday);
+        // 截止时间、提醒时间、规划时间在3天后的任务 （第1、2个在 三天内截止中）（3、4在最近创建）
+        TaskDTO taskOfFullTimeInfoOfAfterThreeDays = saveUncompletedTask(beforeThreeDaysTimestamp, afterThreeDaysTimestamp, afterThreeDays, afterThreeDays);
+        TaskDTO taskEndAfterThreeDays = saveUncompletedTask(beforeThreeDaysTimestamp, null, null, afterThreeDays);
+        TaskDTO taskRemindAfterThreeDays = saveUncompletedTask(beforeThreeDaysTimestamp, afterThreeDaysTimestamp, null, null);
+        TaskDTO taskExpectedAfterThreeDays = saveUncompletedTask(beforeThreeDaysTimestamp, null, afterThreeDays, null);
+        // 截止时间、提醒时间、规划时间在4天后的任务 （第1、2个在 4-7天截止中）（其余不在）
+        TaskDTO taskOfFullTimeInfoOfAfterFourDays = saveUncompletedTask(beforeFourDaysTimestamp, afterFourDaysTimestamp, afterFourDays, afterFourDays);
+        TaskDTO taskEndAfterFourDays = saveUncompletedTask(beforeFourDaysTimestamp, null, null, afterFourDays);
+        TaskDTO taskRemindAfterFourDays = saveUncompletedTask(beforeFourDaysTimestamp, afterFourDaysTimestamp, null, null);
+        TaskDTO taskExpectedAfterFourDays = saveUncompletedTask(beforeFourDaysTimestamp, null, afterFourDays, null);
+
+        // 已经完成的任务，均不在推荐中
+        saveCompletedTask(todayTimestamp, todayTimestamp, today, today);
+        saveCompletedTask(todayTimestamp, todayTimestamp, null, null);
+        saveCompletedTask(todayTimestamp, null, today, null);
+        saveCompletedTask(todayTimestamp, null, null, today);
+        saveCompletedTask(todayTimestamp, tomorrowTimestamp, tomorrow, tomorrow);
+        saveCompletedTask(todayTimestamp, tomorrowTimestamp, null, null);
+        saveCompletedTask(todayTimestamp, null, tomorrow, null);
+        saveCompletedTask(todayTimestamp, null, null, tomorrow);
+        saveCompletedTask(yesterdayTimestamp, yesterdayTimestamp, yesterday, yesterday);
+        saveCompletedTask(yesterdayTimestamp, yesterdayTimestamp, null, null);
+        saveCompletedTask(yesterdayTimestamp, null, yesterday, null);
+        saveCompletedTask(yesterdayTimestamp, null, null, yesterday);
+        saveCompletedTask(beforeThreeDaysTimestamp, afterThreeDaysTimestamp, afterThreeDays, afterThreeDays);
+        saveCompletedTask(beforeThreeDaysTimestamp, null, null, afterThreeDays);
+        saveCompletedTask(beforeThreeDaysTimestamp, afterThreeDaysTimestamp, null, null);
+        saveCompletedTask(beforeThreeDaysTimestamp, null, afterThreeDays, null);
+        saveCompletedTask(beforeFourDaysTimestamp, afterFourDaysTimestamp, afterFourDays, afterFourDays);
+        saveCompletedTask(beforeFourDaysTimestamp, null, null, afterFourDays);
+        saveCompletedTask(beforeFourDaysTimestamp, afterFourDaysTimestamp, null, null);
+        saveCompletedTask(beforeFourDaysTimestamp, null, afterFourDays, null);
 
         // When
         RecommendMyDayDTO recommendMyDayDTO = myDayTaskService.getRecommendTasks();
+        RecommendTaskListDTO tasksEndInThreeDays = recommendMyDayDTO.getTasksEndInThreeDays();
+        RecommendTaskListDTO tasksEndInFourToSevenDays = recommendMyDayDTO.getTasksEndInFourToSevenDays();
+        RecommendTaskListDTO uncompletedTasksBeforeToday = recommendMyDayDTO.getUncompletedTasksBeforeToday();
+        RecommendTaskListDTO latestCreatedTasks = recommendMyDayDTO.getLatestCreatedTasks();
 
         // Then
+        assertNotNull("建议中应该包含三天内截止的任务", tasksEndInThreeDays);
+        assertEquals("三天内截止的任务数量不正确", 4, tasksEndInThreeDays.getTaskDTOList().size());
+        assertTrue("三天内截止的任务应该包含taskEndTomorrow", tasksEndInThreeDays.getTaskDTOList().contains(taskEndTomorrow));
+        assertTrue("三天内截止的任务应该包含taskOfFullTimeInfoOfTomorrow", tasksEndInThreeDays.getTaskDTOList().contains(taskOfFullTimeInfoOfTomorrow));
+        assertTrue("三天内截止的任务应该包含taskOfFullTimeInfoOfAfterThreeDays", tasksEndInThreeDays.getTaskDTOList().contains(taskOfFullTimeInfoOfAfterThreeDays));
+        assertTrue("三天内截止的任务应该包含taskEndAfterThreeDays", tasksEndInThreeDays.getTaskDTOList().contains(taskEndAfterThreeDays));
 
+        assertNotNull("建议中应该包含四到七天内的任务", tasksEndInFourToSevenDays);
+        assertEquals("四到七天内截止的任务数量不正确", 2, tasksEndInFourToSevenDays.getTaskDTOList().size());
+        assertTrue("四到七天内截止的任务为 taskOfFullTimeInfoOfAfterFourDays", tasksEndInFourToSevenDays.getTaskDTOList().contains(taskOfFullTimeInfoOfAfterFourDays));
+        assertTrue("四到七天内截止的任务为 taskEndAfterFourDays", tasksEndInFourToSevenDays.getTaskDTOList().contains(taskEndAfterFourDays));
+
+        assertNotNull("建议中应该包含过去的任务", uncompletedTasksBeforeToday);
+        assertEquals("过去的任务数量不正确", 4, uncompletedTasksBeforeToday.getTaskDTOList().size());
+        assertTrue("过去的任务应该包含taskOfFullTimeInfoOfYesterday", uncompletedTasksBeforeToday.getTaskDTOList().contains(taskOfFullTimeInfoOfYesterday));
+        assertTrue("过去的任务应该包含taskRemindYesterday", uncompletedTasksBeforeToday.getTaskDTOList().contains(taskRemindYesterday));
+        assertTrue("过去的任务应该包含taskExpectedYesterday", uncompletedTasksBeforeToday.getTaskDTOList().contains(taskExpectedYesterday));
+        assertTrue("过去的任务应该包含taskEndYesterday", uncompletedTasksBeforeToday.getTaskDTOList().contains(taskEndYesterday));
+
+        assertNotNull("建议中应该包含最近(三天)创建的任务", latestCreatedTasks);
+        assertEquals("最近(三天)创建的任务数量不正确，得到的任务id列表：" + latestCreatedTasks.getTaskDTOList().stream().map(TaskDTO::getId).toList(), 4, latestCreatedTasks.getTaskDTOList().size());
+        assertTrue("最近(三天)创建的任务中应该包含taskRemindTomorrow", latestCreatedTasks.getTaskDTOList().contains(taskRemindTomorrow));
+        assertTrue("最近(三天)创建的任务中应该包含taskExpectedTomorrow", latestCreatedTasks.getTaskDTOList().contains(taskExpectedTomorrow));
+        assertTrue("最近(三天)创建的任务中应该包含taskRemindAfterThreeDays", latestCreatedTasks.getTaskDTOList().contains(taskRemindAfterThreeDays));
+        assertTrue("最近(三天)创建的任务中应该包含taskExpectedAfterThreeDays", latestCreatedTasks.getTaskDTOList().contains(taskExpectedAfterThreeDays));
     }
 }
