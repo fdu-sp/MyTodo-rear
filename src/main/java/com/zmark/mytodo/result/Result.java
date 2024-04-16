c0596fe73d32e66885102fe12d9b537b62a88f7d
zmark <351479188@qq.com>
2024-04-16 14:15:23 +0800

    refactor:修改result类

--- src/main/java/com/zmark/mytodo/result/Result.java
+++ src/main/java/com/zmark/mytodo/result/Result.java
@@ -13,11 +13,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Result {
     private int code;
+    /**
+     * 表示Result是否按照协议进行
+     */
+    private boolean uxApi;
+    /**
+     * 请求是否成功
+     */
+    private boolean success;
     private String msg;
     private Object object;
 
-    public Result(int code, String msg, Object object) {
+    public Result(int code, boolean success, String msg, Object object) {
         this.code = code;
+        this.uxApi = true;
+        this.success = success;
         this.msg = msg;
         this.object = object;
     }
--- src/main/java/com/zmark/mytodo/result/ResultFactory.java
+++ src/main/java/com/zmark/mytodo/result/ResultFactory.java
@@ -8,44 +8,44 @@ package com.zmark.mytodo.result;
  */
 public class ResultFactory {
 
-    public static Result buildResult(Integer resultCode, String msg, Object data) {
-        return new Result(resultCode, msg, data);
+    public static Result buildResult(Integer resultCode, boolean success, String msg, Object data) {
+        return new Result(resultCode, success, msg, data);
     }
 
-    public static Result buildResult(ResultCode resultCode, String msg, Object data) {
-        return new Result(resultCode.getCode(), msg, data);
+    public static Result buildResult(ResultCode resultCode, boolean success, String msg, Object data) {
+        return new Result(resultCode.getCode(), success, msg, data);
     }
 
     public static Result buildSuccessResult(Object data) {
-        return buildResult(ResultCode.SUCCESS, "成功", data);
+        return buildResult(ResultCode.SUCCESS, true, "成功", data);
     }
 
     public static Result buildSuccessResult() {
-        return buildResult(ResultCode.SUCCESS, "成功", null);
+        return buildResult(ResultCode.SUCCESS, true, "成功", null);
     }
 
     public static Result buildSuccessResult(String msg, Object data) {
-        return buildResult(ResultCode.SUCCESS, msg, data);
+        return buildResult(ResultCode.SUCCESS, true, msg, data);
     }
 
     public static Result buildFailResult(String msg) {
-        return buildResult(ResultCode.FAIL, msg, null);
+        return buildResult(ResultCode.FAIL, false, msg, null);
     }
 
     public static Result buildInternalServerErrorResult() {
-        return buildResult(ResultCode.INTERNAL_SERVER_ERROR, "服务器开小差啦~", null);
+        return buildResult(ResultCode.INTERNAL_SERVER_ERROR, false, "服务器开小差啦~", null);
     }
 
     public static Result buildInsufficientPermissionsResult() {
-        return buildResult(ResultCode.UNAUTHORIZED, "您的权限不足！", null);
+        return buildResult(ResultCode.UNAUTHORIZED, false, "您的权限不足！", null);
     }
 
     public static Result buildInsufficientPermissionsResult(String msg) {
-        return buildResult(ResultCode.UNAUTHORIZED, msg, null);
+        return buildResult(ResultCode.UNAUTHORIZED, false, msg, null);
     }
 
     public static Result buildNotFoundResult(String msg) {
-        return buildResult(ResultCode.NOT_FOUND, msg, null);
+        return buildResult(ResultCode.NOT_FOUND, false, msg, null);
     }
 
 }
