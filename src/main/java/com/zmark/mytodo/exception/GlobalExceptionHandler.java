74759a84101cf0eb1c8e6ab4254fc88d52cb5bd8
zmark <351479188@qq.com>
2024-04-19 22:04:16 +0800

    fix(GlobalExceptionHandler): 请求参数校验失败的返回值
    结合前端约定，HTTP状态码返回200，业务状态码返回400

--- src/main/java/com/zmark/mytodo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zmark/mytodo/exception/GlobalExceptionHandler.java
@@ -3,14 +3,13 @@ package com.zmark.mytodo.exception;
 
 import com.zmark.mytodo.result.Result;
 import com.zmark.mytodo.result.ResultFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.stream.Collectors;
@@ -19,11 +18,11 @@ import java.util.stream.Collectors;
  * @author ZMark
  * @date 2023/12/5 14:53
  */
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     public ResponseEntity<Result> handleValidationExceptions(MethodArgumentNotValidException ex) {
         BindingResult bindingResult = ex.getBindingResult();
@@ -31,7 +30,9 @@ public class GlobalExceptionHandler {
                 .stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.joining("; "));
+        log.warn("Validation error: {}", errorMessage);
+        // 结合前端约定，HTTP状态码返回200，业务状态码返回400
         Result result = ResultFactory.buildFailResult(errorMessage);
-        return ResponseEntity.badRequest().body(result);
+        return ResponseEntity.accepted().body(result);
     }
 }
