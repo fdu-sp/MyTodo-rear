1a2b68e97d089e30161076af9fc588dab9c66662
Chen Peiyi <cpyyy_0614@163.com>
2024-05-05 00:14:16 +0800

    docs(ITimerService): add comments of method createNewTimer

--- src/main/java/com/zmark/mytodo/service/api/ITimerService.java
+++ src/main/java/com/zmark/mytodo/service/api/ITimerService.java
@@ -11,6 +11,14 @@ import org.springframework.transaction.annotation.Transactional;
  * @date 2024/5/4 15:17
  */
 public interface ITimerService {
+
+    /**
+     * 如果创建成功，返回新创建的计时器DTO
+     *
+     * @param timerCreateReq 创建计时器的请求
+     * @throws NewEntityException 如果创建失败，抛出异常（当前已有计时器存在，或者对已完成的任务创建计时器）
+     * @throws NoDataInDataBaseException 如果关联的任务不存在，抛出异常
+     */
     @Transactional
-    TimerDTO createNewTimer(TimerCreateReq createReq) throws NewEntityException, NoDataInDataBaseException;
+    TimerDTO createNewTimer(TimerCreateReq timerCreateReq) throws NewEntityException, NoDataInDataBaseException;
 }
